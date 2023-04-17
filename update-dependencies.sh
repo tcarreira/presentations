9f6206c9109f705854ed463646ccd7fb57d1a794
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-04-17 14:00:27 -0300

    update jquery

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -4,5 +4,5 @@
 {
 cd "$1" || exit 5
 curl https://remarkjs.com/downloads/remark-latest.min.js -s -o remark-latest.min.js
-curl https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js -s -o jquery.min.js
+curl https://ajax.googleapis.com/ajax/libs/jquery/3.5.0/jquery.min.js -s -o jquery.min.js
 }
